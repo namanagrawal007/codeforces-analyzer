dc0de2f4e7b77cc5485f8d98d72d97c44569a30f
Naman Agrawal <namanagrawal190503@gmail.com>
2025-06-26 00:43:51 +0530

    Initial commit by Naman Agrawal

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span>
+                Developed by <a href="https://codeforces.com/profile/justnaman"  target="_blank">Naman Agrawal</a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
